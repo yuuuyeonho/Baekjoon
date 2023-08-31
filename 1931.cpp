e3bdfaf025e7b06a3d087708a185001622a62f53
YuYeonho <yuyeonho0619@gmail.com>
2023-09-01 01:26:16 +0900

    Create 1931.cpp

---
+++ 1931.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<pair <int, int>> p;
+
+    for(int i=0; i<n; ++i){
+        int begin, end;
+        cin >> begin >> end;
+        //sort 함수는 pair의 first를 기준으로 정렬한다. 끝나는 시간순으로 정렬이 필요하므로 end를 first에 저장
+        p.push_back(make_pair(end, begin)); 
+    }
+
+    sort(p.begin(),p.end());
+
+    int time = p[0].first;
+    int count = 1;
+    for(int i=0; i<n; ++i){
+        if(time <= p[i].second) // second가 시작하는 시간이므로 시작 시간이 앞회의 끝나느 시간보다 크면
+        {
+            count++;
+            time = p[i].first;
+        }
+    }
+
+    cout << count;
+    
+}
+
+/*
+가장 먼저 끝나는 거 -> 그다음에 시작하는거 중에 가장 먼저 끝나는거 -> 반복해서 구함
+즉, 끝나는 시간이 중요했다.
+*/
