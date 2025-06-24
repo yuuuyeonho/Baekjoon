46efa6ea022a8c03fd423797d123a5cfcc22da51
YuYeonho <yuyeonho0619@gmail.com>
2025-06-24 15:54:22 +0900

    create 1991.cpp

--- 1991.cpp
+++ 1991.cpp
@@ -1,29 +1,58 @@
-#include<iostream>
-#include<string>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
-struct node
-{
-    node* left;
-    node* right;
-    char value;
-};
+pair<int, int> alp[26];
 
-class binarySearchTree{
-    private:
-        node* root;
-    public:
-        
-};
+void preorder(int cur) {
+  if (cur == -1) return;
+  char ch = 'A' + cur;
+  cout << ch;
+  preorder(alp[cur].first);
+  preorder(alp[cur].second);
+}
 
-string preorder(){}
+void inorder(int cur) {
+  if (cur == -1) return;
+  inorder(alp[cur].first);
+  char ch = 'A' + cur;
+  cout << ch;
+  inorder(alp[cur].second);
+}
 
-string inorder(){}
+void postorder(int cur) {
+  if (cur == -1) return;
+  postorder(alp[cur].first);
+  postorder(alp[cur].second);
+  char ch = 'A' + cur;
+  cout << ch;
+}
 
-string postorder(){}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n;
+  char x, y, z;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> x >> y >> z;
+    if (y == '.')
+      alp[x - 'A'].first = -1;
+    else
+      alp[x - 'A'].first = y - 'A';
 
-int main(){
-    char ch;
-    cin >> ch;
+    if (z == '.')
+      alp[x - 'A'].second = -1;
+    else
+      alp[x - 'A'].second = z - 'A';
+  }
 
+  preorder(0);
+  cout << '\n';
+  inorder(0);
+  cout << '\n';
+  postorder(0);
+
+  return 0;
 }
