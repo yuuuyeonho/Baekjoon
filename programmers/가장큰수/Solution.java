d71288b8b4751f4bb2fd880dee7f16b14f69e541
YuYeonho <yuyeonho0619@gmail.com>
2026-06-06 20:34:09 +0900

    add files

--- programmers/가장큰수/Solution.java
+++ programmers/가장큰수/Solution.java
@@ -1,8 +1,26 @@
+
+import java.util.Arrays;
+
 class Solution {
     public String solution(int[] numbers) {
+        String[] numStr = new String[numbers.length];
+
+        for (int i=0; i < numbers.length; i++) {
+            numStr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(numStr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (numStr[0].equals("0")) {
+            return "0";
+        }
+
         String answer = "";
+
+        for (String str : numStr) {
+            answer += str;
+        }
+
         return answer;
     }
 }
-
-// 첫번째 자리 계산 -> 
